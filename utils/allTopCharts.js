38a7568c0390f91d2d89e4d9e5159fda442a7052
Patrick Trasborg <patrick.trasborg@gmail.com>
2016-02-26 17:12:39 -0500

    starting top charts

---
+++ utils/allTopCharts.js
@@ -0,0 +1,1 @@
+curl --data "ajax=1&geo=US&date=201310" http://www.google.com/trends/topcharts/category
---
+++ utils/categoryTopCharts.js
@@ -0,0 +1,1 @@
+curl --data "ajax=1&cid=actors&geo=US&date=201310" http://www.google.com/trends/topcharts/trendingchart
