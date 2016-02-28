6be85498bf31e10942b8592553827e5a47df19a9
Patrick Trasborg <patrick.trasborg@gmail.com>
2016-02-28 17:33:23 -0500

    added top30in30 test file

---
+++ test/utils/top30in30.test.js
@@ -0,0 +1,11 @@
+var chai = require('chai');
+var chaiAsPromised = require('chai-as-promised');
+chai.use(chaiAsPromised);
+var should = chai.should();
+
+var top30in30 = require('../../lib/utils/top30in30.js');
+
+module.exports = 
+describe('top30in30.js', function(){
+	it('need to add tests');
+});
