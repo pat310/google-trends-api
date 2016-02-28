54238724366d956e9b10f7a8c500f432ca20ac09
Patrick Trasborg <patrick.trasborg@gmail.com>
2016-02-28 17:32:42 -0500

    added categoryTopCharts tests

---
+++ test/utils/categoryTopCharts.test.js
@@ -0,0 +1,19 @@
+var chai = require('chai');
+var chaiAsPromised = require('chai-as-promised');
+chai.use(chaiAsPromised);
+var should = chai.should();
+
+var categoryTopCharts = require('../../lib/utils/categoryTopCharts.js');
+
+module.exports = 
+describe('categoryTopCharts.js', function(){
+	it('should reject if category is not provided', function(){
+		return categoryTopCharts().should.be.rejectedWith('Category must be provided');
+	});
+	it('should reject if date is invalid', function(){
+		return categoryTopCharts('dogs','201413').should.be.rejectedWith('Date is invalid');
+	});
+	it('should reject if country is invalid', function(){
+		return categoryTopCharts('dogs','201401', 'ZZ').should.be.rejectedWith('Could not locate country');
+	});
+});
