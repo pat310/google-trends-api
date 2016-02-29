68aafe5ab8c43dc404a77d4e76400ff0f379ffca
Patrick Trasborg <patrick.trasborg@gmail.com>
2016-02-29 15:06:50 -0500

    allowing user to optionally pass an object rather than specifc parameters

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 An API layer ontop of google trends
+
+{
+geo: 'CODE',
+date: '201601',
+category: 'Dogs',
+keywords: ['Dogs', 'Cats', 'Bears']
+}
--- index.js
+++ index.js
@@ -2,8 +2,9 @@ require('babel-core/register');
 
 var tools = require('./lib/utils/');
 
-// //Parameters: array of key words (required), country as a string (optional, 'US' is default)
-// tools.topRelated('dog house')
+// // Parameters: array of key words (required), country as a string (optional, 'US' is default)
+// // optionally as the first argument pass an object: {keywords: ['dog'], geo: 'US'}
+// tools.topRelated({keywords: 'dog'})
 // .then(function(topRelated){
 // 	console.log("here are the topRelated", topRelated);
 // })
@@ -13,7 +14,8 @@ var tools = require('./lib/utils/');
 // });
 
 // //Parameters: takes a country as a string (optional, 'US' is default)
-// tools.hotTrends()
+// // optionally as the first argument pass an object: {geo: 'US'}
+// tools.hotTrends({geo: 'JP'})
 // .then(function(results){
 // 	console.log("these are the results", results);
 // })
@@ -27,8 +29,9 @@ var tools = require('./lib/utils/');
 // 	console.log("these are the results", results);
 // });
 
-// //Parameters: date in format yyyymm where January is 01 (optional, today's date is default), country code as string (optional, 'US' is default)
-// tools.allTopCharts()
+// // Parameters: date in format yyyymm where January is 01 (optional, today's date is default), country code as string (optional, 'US' is default)
+// // optionally as the first argument pass an object: {country: 'US', date: '201601'}
+// tools.allTopCharts({country: 'US', date: '201601'})
 // .then(function(results){
 // 	console.log('got your results here', results);
 // })
@@ -36,14 +39,15 @@ var tools = require('./lib/utils/');
 // 	console.log("there was an error", err);
 // });
 
-//Parameters: category (required), date in format yyyymm where January is 01 (optional, today's date is default), country code as string (optional, 'US' is default)
-tools.categoryTopCharts('actors')
-.then(function(results){
-	console.log('results here', results);
-})
-.catch(function(err){
-	console.log('there was an error', err);
-});
+// //Parameters: category (required), date in format yyyymm where January is 01 (optional, today's date is default), country code as string (optional, 'US' is default)
+// // optionally as the first argument pass an object: {category: 'actors', geo: 'US', date:'201601'}
+// tools.categoryTopCharts({category: 'actors', geo: 'US', date:'201601'})
+// .then(function(results){
+// 	console.log('results here', results);
+// })
+// .catch(function(err){
+// 	console.log('there was an error', err);
+// });
 
 // module.exports = require('./lib/utils/');
 // console.log('now here');
