343756b7d66ceb99e4a8fffecc4573852402a1a7
pat310 <patrick.trasborg@gmail.com>
2016-10-22 14:54:58 -0400

    adding an optional parameter 'timePeriod' to 'trendData' method

--- src/utils/trendData.js
+++ src/utils/trendData.js
@@ -6,14 +6,15 @@ var checkErrors = require(__dirname + '/../resources/errorHandling.js');
 var parseJSON = require(__dirname + '/../resources/htmlParser.js').parseJSON;
 var groupKeywords = require(__dirname + '/../resources/trendDataHelper.js').groupKeywords;
 var reduceArrayDimensions = require(__dirname + '/../resources/trendDataHelper.js').reduceArrayDimensions;
+var timePeriodConverter = require(__dirname + '/../resources/timePeriodConverter.js');
 
-module.exports = function request(keywords, cbFunc){
+module.exports = function request(keywords, timePeriod, cbFunc){
 	var obj = createObj(arguments, request);
 
 	var error = checkErrors(obj);
 	if(error instanceof Error) return Promise.reject(obj.cbFunc(error));
 
-	return Promise.all(promiseArr(obj.keywords))
+	return Promise.all(promiseArr(obj.keywords, timePeriodConverter(obj.timePeriod)))
 	.then(function(results){
 		return obj.cbFunc(null, reduceArrayDimensions(results));
 	})
@@ -22,9 +23,9 @@ module.exports = function request(keywords, cbFunc){
 	});
 };
 
-function promiseArr(keywords){
+function promiseArr(keywords, timePeriod){
 	return groupKeywords(keywords).map(function(keyword, index, arr){
-		return rp(`http://www.google.com/trends/fetchComponent?q=${keyword}&cid=TIMESERIES_GRAPH_0&export=3`)
+		return rp(`http://www.google.com/trends/fetchComponent?q=${keyword}&cid=TIMESERIES_GRAPH_0&export=3&${timePeriod}`)
 		.then(function(htmlString){
 			return parseJSON(htmlString, arr[index].split(','));
 		});
