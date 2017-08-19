fd0efa4557822023f795f4cd8af05045e8aeae7f
pat310 <patrick.trasborg@gmail.com>
2017-08-19 14:00:16 -0400

    adding test case and example, updating README

--- examples.js
+++ examples.js
@@ -1,19 +1,31 @@
 'use strict';
 
-var googleTrends = require('./lib/google-trends-api.js');
-var util = require('util');
+var googleTrends = require('./lib/google-trends-api.min.js');
+
+/******************** Autocomplete **************************/
+
+// googleTrends.autoComplete({keyword: 'Back to school'})
+// .then((res) => {
+//   console.log('this is res', res);
+// })
+// .catch((err) => {
+//   console.log('got the error', err);
+//   console.log('error message', err.message);
+//   console.log('request body',  err.requestBody);
+// });
+
 
 /******************** Interest over time **************************/
 
-googleTrends.autoComplete({keyword: 'Back to school'})
-.then((res) => {
-  console.log('this is res', res);
-})
-.catch((err) => {
-  console.log('got the error', err);
-  console.log('error message', err.message);
-  console.log('request body',  err.requestBody);
-});
+// googleTrends.interestOverTime({keyword: 'Valentines Day'})
+// .then((res) => {
+//   console.log('this is res', res);
+// })
+// .catch((err) => {
+//   console.log('got the error', err);
+//   console.log('error message', err.message);
+//   console.log('request body',  err.requestBody);
+// });
 
 // googleTrends.interestOverTime({keyword: 'Valentines Day', startTime: new Date(Date.now() - (4 * 60 * 60 * 1000))}, function(err, results) {
 //   if (err) console.log('oh no error!', err);
